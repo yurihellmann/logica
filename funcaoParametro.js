0a485e61fb0026e4d15bf63a26ebf81b1c3e72e3
yurihellmann <yurihellmann88@gmail.com>
2024-07-05 16:53:44 -0300

    funcoes

---
+++ funcaoParametro.js
@@ -0,0 +1,13 @@
+// torrar("pão de forma", "yuri")
+// torrar("pão integral", "yuri")
+
+// function torrar(pao, nome){
+//     console.log("torrada feita com " + pao)
+//     console.log("ela é um pedido de " + nome)
+// }
+
+createStringConnection("db_products")
+
+function createStringConnection(databaseName){
+    console.log("string de conexão ao banco...")
+}
